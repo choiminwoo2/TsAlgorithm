2b0681189e70c338087c6af2471fd71f94d5cbbf
wjrdjs005 <wjrdjs005@naver.com>
2023-01-06 00:06:03 +0900

    병합정렬주석추가

--- domain/mergeSort.ts
+++ domain/mergeSort.ts
@@ -2,6 +2,7 @@ const merge = (firArr : Array<number>, secArr : Array<number>) =>{
     let result : Array<number>= [];
     let i = 0;
     let j = 0;
+    //두 배열을 비교하여 순차적으로 새 배열에 push.
     while( i < firArr.length && j < secArr.length){
         if(secArr[j] > firArr[i]){
             result.push(firArr[i]);
@@ -11,10 +12,12 @@ const merge = (firArr : Array<number>, secArr : Array<number>) =>{
             j++;
         }
     }
+    // j가 i보다 작은 경우 배열을 타지 못하면 firArr배열이 루프를 타지 못하여 처리해야함.
     while(i < firArr.length){
         result.push(firArr[i]);
         i++;
     }
+    //  i가 j보다 작은 경우 배열을 타지 못하면  secArr 배열이 루프를 타지 못하여 처리해야함.
     while(j < secArr.length){
         result.push(secArr[j]);
         j++;
@@ -22,6 +25,7 @@ const merge = (firArr : Array<number>, secArr : Array<number>) =>{
     return result;
 }
 
+// 재귀 호출을 사용하여 배열이 1이하까지 줄여질 때 까지 반복하고 새 배열을 만들어준다.
 export const mergeSort = (arr : Array<number>) : Array<number>=>{
     if(arr.length <= 1) return arr;
     let middlePoint =  Math.floor(arr.length / 2);
